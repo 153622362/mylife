3c64483b1a7bad3547815d21c922e4525fcdf505
ngyhd <153622362@qq.com>
2018-12-26 19:07:34 +0800

    update

---
+++ _ide_helper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ngyhd
+ * Date: 2018/12/26
+ * Time: 17:31
+ */
+
+/**
+ * phpstorm自动补全
+ * Class Yii
+ */
+class Yii
+{
+	/**
+	 * @var MyApplication
+	 */
+	public static $app;
+}
+
+/**
+ * @property yii\redis\Connection  $redis
+ */
+class MyApplication
+{
+}
